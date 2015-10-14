db3fb7c792f4477154c63888fe6eb4f51d0b3e7a
Kyle Bennett <kyle.j.bennett5@gmail.com>
2015-10-14 10:47:51 -0400

    added map

---
+++ config/initializers/yelp.rb
@@ -0,0 +1,8 @@
+require 'yelp'
+
+Yelp.client.configure do |config|
+  config.consumer_key = "eKwR7oGZ7_ToZPjGEmiBeA"
+  config.consumer_secret = "cH_Fg1__t32wcsjjgEMyYmNKUH8"
+  config.token = "-T96vIteNA5Rnft2862ol9IYaP7Cqtpc"
+  config.token_secret = "wGQ_kHZgjz46HsM8sm5Vr1zZ9SA"
+end
