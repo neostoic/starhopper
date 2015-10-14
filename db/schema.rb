34824c75c3f172f8d732e24460f49c40d99a7db2
Zach Krzyzanowski <zkrzyz@gmail.com>
2015-10-14 17:32:26 -0400

    Fixed profile link in navbar

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151014195328) do
+ActiveRecord::Schema.define(version: 20151014210750) do
 
   create_table "profiles", force: :cascade do |t|
     t.integer  "user_id"
